34c9b845406930260814d6ccc8563b9c5da8b2f7
Luca Capra <luca.capra@gmail.com>
2015-08-25 17:25:22 +0200

    up node-red 0.11.1, fix md5 dep, fix palette

--- build-docker.sh
+++ build-docker.sh
@@ -2,20 +2,23 @@ if [ ! -e node_modules/node-red ]; then
 	npm i
 fi;
 
+cd docker;
 
-cd docker; 
-
+cp /etc/hosts ./
 
 mkdir node-red
 
 cp -r ../node_modules/node-red/bin ./node-red/
 cp -r ../node_modules/node-red/nodes ./node-red/
+cp -r ../node_modules/node-red/locales ./node-red/
+cp -r ../node_modules/node-red/editor ./node-red/
 cp -r ../node_modules/node-red/public ./node-red/
 cp -r ../node_modules/node-red/red ./node-red/
 cp  ../node_modules/node-red/red.js ./node-red/
 cp  ../node_modules/node-red/package.json ./node-red/
 
+
 docker build -t muka/redzilla .
 
 rm node-red -r
-
+rm hosts
